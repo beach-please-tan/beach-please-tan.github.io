2d10974be2c0eb7db744b9477b0cbe4ac9d33fad
bodi-cmd <bogdan.barbu.dev@gmail.com>
2021-12-24 14:26:26 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$( document ).ready(function() {
+        $('.join-button').click(function() {
+            $('html, body').animate({
+                scrollTop: $('#contact-us').offset().top
+            }, 1000);
+        });
+
+        $('#about-us-btn').click(function() {
+            $('html, body').animate({
+                scrollTop: $('#about-us').offset().top
+            }, 1000);
+        });
+
+        $('#contact-btn').click(function() {
+            $('html, body').animate({
+                scrollTop: $('#contact-us').offset().top
+            }, 1000);
+        });
+})
